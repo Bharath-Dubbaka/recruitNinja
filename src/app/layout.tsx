3a8309030f4dee846488d0bfdf6ffc4330f5c91e
Bharath Dubbaka <bharath.reactdev@gmail.com>
2024-12-12 11:57:02 +0530

    added favicon and added metadata with opengraph details as well

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,9 +7,25 @@ import Footer from "@/components/ui/footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-   title: "recruitCatch - LinkedIn X-Ray Search Tool",
+   title: "RecruitCatch - LinkedIn X-Ray Search Tool",
    description:
-      "Made in India, X Ray search tool to Search LinkedIn profiles using advanced Boolean strings",
+      "X Ray search tool (Made in India) to Search LinkedIn profiles using advanced Boolean strings",
+   openGraph: {
+      type: "website",
+      url: "https://RecruitCatch.com/", // Replace with your actual website URL
+      title: "RecruitCatch - Xray search tool for social/public profiles",
+      description:
+         "X Ray search tool to Search LinkedIn profiles using advanced Boolean strings.",
+      siteName: "RecruitCatch",
+      images: [
+         {
+            url: "https://recruitcatch.com//opengraph-image.png", // Replace with your actual OG image URL
+            width: 1200,
+            height: 630,
+            alt: "RecruitCatch - Xray search tool for social/public profiles",
+         },
+      ],
+   },
 };
 
 export default function RootLayout({
