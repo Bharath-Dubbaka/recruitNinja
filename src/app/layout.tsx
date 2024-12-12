542d20e6c646d5607bc20ea5244058384b4b2d85
Bharath Dubbaka <bharath.reactdev@gmail.com>
2024-12-12 12:42:38 +0530

    added sitemap and robots files for better crawling and seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Roboto } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/ui/navbar";
 import Footer from "@/components/ui/footer";
---
+++ src/app/sitemap.ts
@@ -0,0 +1,36 @@
+import type { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+   return [
+      {
+         url: "https://recruitcatch.com/",
+         lastModified: new Date(),
+         changeFrequency: "yearly",
+         priority: 1,
+      },
+      //   {
+      //      url: "https://cvtosalary.com/about",
+      //      lastModified: new Date(),
+      //      changeFrequency: "monthly",
+      //      priority: 0.8,
+      //   },
+      {
+         url: "https://recruitcatch.com/contact",
+         lastModified: new Date(),
+         changeFrequency: "weekly",
+         priority: 0.8,
+      },
+      {
+         url: "https://recruitcatch.com/privacy",
+         lastModified: new Date(),
+         changeFrequency: "weekly",
+         priority: 0.8,
+      },
+      {
+         url: "https://recruitcatch.com/tos",
+         lastModified: new Date(),
+         changeFrequency: "weekly",
+         priority: 0.8,
+      },
+   ];
+}
